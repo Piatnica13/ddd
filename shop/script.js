108048557fac9925ab7a52dbb9dd925591ccdcb2
Gorkovenko Konstantin <konstantin.gorkovenko13@gmail.com>
2025-01-05 22:27:40 +0500

    ить бр

--- shop/script.js
+++ shop/script.js
@@ -2,7 +2,7 @@ let MainContener = document.querySelector('#Body');
 console.log(`dfdf`);
 
 document.addEventListener("DOMContentLoaded", () => {
-  fetch('/menu/index.html')
+  fetch('../menu/index.html')
   .then(response => response.text())
   .then(html => {
     document.getElementById('menu-placeholder').innerHTML = html;
